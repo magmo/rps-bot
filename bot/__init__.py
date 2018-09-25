925046577d7e8d0d23bc7698ab244f39a3b0c4df
Mike Kerzhner <mikerzhner@gmail.com>
2018-09-25 12:01:46 -0700

    Only initializing firebase app once

--- bot/__init__.py
+++ bot/__init__.py
@@ -7,10 +7,14 @@ from firebase_admin import db
 def create_app(test_config=None):
     app = Flask(__name__)
     app.config.update(test_config)
-    firebase_admin.initialize_app(options={
-        'databaseURL': 'https://rock-paper-scissors-dev.firebaseio.com',
-        'projectId': 'rock-paper-scissors-dev'
-    })
+
+    try:
+        firebase_admin.get_app()
+    except ValueError:
+        firebase_admin.initialize_app(options={
+            'databaseURL': 'https://rock-paper-scissors-dev.firebaseio.com',
+            'projectId': 'rock-paper-scissors-dev'
+        })
 
     def set_db():
         g.db = db.reference()
